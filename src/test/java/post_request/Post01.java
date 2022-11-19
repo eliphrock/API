a826c1536826c48b297e5b8b6214afdf8eb5fbc8
eliphrock <eliphrock@gmail.com>
2022-11-19 13:53:33 -0500

    second commit

--- src/test/java/post_request/Post01.java
+++ src/test/java/post_request/Post01.java
@@ -4,11 +4,14 @@ import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Post01 extends JsonPlaceHolderBaseUrl {
 /*
@@ -43,11 +46,16 @@ public class Post01 extends JsonPlaceHolderBaseUrl {
 
         //set the expected data
 
-        Map<String,Object> expectedData=new HashMap<>();
-        expectedData.put("userId",55);
-        expectedData.put("title","Tidy your room");
-        expectedData.put("completed",false);
-        expectedData.put("id",201);
+        //Map<String,Object> expectedData=new HashMap<>();
+        //expectedData.put("userId",55);
+       // expectedData.put("title","Tidy your room");
+      //  expectedData.put("completed",false);
+       // expectedData.put("id",201);
+
+        //2nd.way from JsonPlaceHolderTestData class
+
+        JsonPlaceHolderTestData obj=new JsonPlaceHolderTestData();
+        Map<String,Object> expectedData=obj.expectedDataJPH(55,"Tidy your room",false);
 
         System.out.println("expectedData =" +expectedData);
 
@@ -58,6 +66,17 @@ public class Post01 extends JsonPlaceHolderBaseUrl {
        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
 
        response.prettyPrint();
+
+
+       //do assertion
+        Map<String,Object> actualData=response.as(HashMap.class);
+
+        System.out.println("actualData =" +actualData);
+
+        assertEquals(201,response.statusCode());
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
     }
 
 }
