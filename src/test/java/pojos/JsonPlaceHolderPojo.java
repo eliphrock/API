e51a53866e4e3b3d1bc6f4aa0f637808299ec664
eliphrock <eliphrock@gmail.com>
2022-11-21 21:55:12 -0500

    commit again

--- src/test/java/pojos/JsonPlaceHolderPojo.java
+++ src/test/java/pojos/JsonPlaceHolderPojo.java
@@ -1,5 +1,9 @@
 package pojos;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.junit.Ignore;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JsonPlaceHolderPojo {
 
     /*
@@ -13,8 +17,60 @@ public class JsonPlaceHolderPojo {
     /*
 
     1)Create private variables for all keys
-    2)create constructors with all keys and without any key(default contsructor)
+    2)create constructors with all parameters and without any parameter(default contsructor)
     3) create public getter and setters methods
+    4)Create toString() method
 
      */
+
+
+    private Integer userId;
+    private String title;
+    private Boolean completed;
+
+    public JsonPlaceHolderPojo(Integer userId, String title, Boolean completed) {
+        this.userId = userId;
+        this.title = title;
+        this.completed = completed;
+    }
+    public JsonPlaceHolderPojo(){
+
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonPlaceHolderPojo{" +
+                "userId=" + userId +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+
+
+
+
 }
--- src/test/java/post_request/Post03Pojo.java
+++ src/test/java/post_request/Post03Pojo.java
@@ -1,7 +1,14 @@
 package post_request;
 
 import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Post03Pojo extends JsonPlaceHolderBaseUrl {
 
@@ -34,6 +41,27 @@ Given
         spec.pathParam("first","todos");
 
         //set the expected data with Pojo Clas
+
+        JsonPlaceHolderPojo expectedData=new JsonPlaceHolderPojo(55,"Tidy your room",false);
+        System.out.println("expectedData =" + expectedData);
+
+
+        //send the request get the response
+
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+        response.prettyPrint();
+
+        //do assertion
+
+        JsonPlaceHolderPojo actualData=response.as(JsonPlaceHolderPojo.class); //de serialization
+        System.out.println("actualData =" + actualData);
+
+        assertEquals(201,response.statusCode());
+        assertEquals(expectedData.getUserId(),actualData.getUserId());
+        assertEquals(expectedData.getTitle(),actualData.getTitle());
+        assertEquals(expectedData.getCompleted(),actualData.getCompleted());
+
+
 
 
     }
