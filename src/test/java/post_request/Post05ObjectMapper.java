4761b7e66ff4623c71cc3a14e41b8a286882743d
eliphrock <eliphrock@gmail.com>
2022-11-26 10:23:01 -0500

    new commit 1

---
+++ src/test/java/post_request/Post05ObjectMapper.java
@@ -0,0 +1,79 @@
+package post_request;
+
+import base_urls.DummyApiBaseUrl;
+import org.junit.Test;
+
+public class Post05ObjectMapper extends DummyApiBaseUrl {
+
+    /*
+       URL: https://dummy.restapiexample.com/api/v1/create
+       HTTP Request Method: POST Request
+       Request body: {
+                        "employee_name": "Ali Can",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image"
+                     }
+       Test Case: Type by using Gherkin Language
+       Assert:
+                i) Status code is 200
+                ii) Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Ali Can",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image",
+                            "id": 6344
+                        },
+                        "message": "Successfully! Record has been added."
+                    }
+     */
+
+
+    /*
+    Given
+    https://dummy.restapiexample.com/api/v1/create
+    And
+    {
+                        "employee_name": "Ali Can",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image"
+                     }
+
+                     When
+                     User sends Post request
+                     Then
+                     Status code is 200
+                     And
+                     {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Ali Can",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image",
+                            "id": 6344
+                        },
+                        "message": "Successfully! Record has been added."
+                    }
+
+
+
+     */
+
+    @Test
+    public void post05(){
+        //set the url
+
+        spec.pathParam("first","create");
+
+        //set the expected data
+
+
+
+
+    }
+}
